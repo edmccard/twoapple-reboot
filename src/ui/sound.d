3c7cca9c555ae24c97e9ce568f7b47348d4dd43c
edmccard <edmccard@hotmail.com>
2012-03-14 08:27:21 -0400

    Turn off sound until segfault fixed

--- src/ui/sound.d
+++ src/ui/sound.d
@@ -38,6 +38,7 @@ static import host;
 static this()
 {
     soundCard = new SoundCardNo();
+/+  TODO: fix sound segfault in fillAudio
     if (host.SDL)
     {
         if (SDL_InitSubSystem(SDL_INIT_AUDIO) == -1)
@@ -49,6 +50,7 @@ static this()
         if (checkCard.isActive)
             soundCard = checkCard;
     }
++/
     host.delay.soundCardActive = &soundCard.getIsActive;
     host.delay.soundCardHasEnoughData = &soundCard.hasEnoughData;
 }
