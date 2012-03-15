fdfb1b14159a44cfd186b35667f9787f56ea62e7
Ed McCardell <edmccard@hotmail.com>
2012-03-15 02:21:12 -0400

    Project name change

--- src/twoapple.d
+++ src/twoapple.d
@@ -1,20 +1,20 @@
 //@ //////////////////////////////////////////////////////////////////////////
 //@ Copyright: 2007 Gerald Stocker
 //@
-//@ This file is part of Twoapple.
+//@ This file is part of twoapple-reboot.
 //@
-//@ Twoapple is free software; you can redistribute it and/or modify
+//@ twoapple-reboot is free software; you can redistribute it and/or modify
 //@ it under the terms of the GNU General Public License as published by
 //@ the Free Software Foundation; either version 2 of the License, or
 //@ (at your option) any later version.
 //@
-//@ Twoapple is distributed in the hope that it will be useful,
+//@ twoapple-reboot is distributed in the hope that it will be useful,
 //@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 //@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //@ GNU General Public License for more details.
 //@
 //@ You should have received a copy of the GNU General Public License
-//@ along with Twoapple; if not, write to the Free Software
+//@ along with twoapple-reboot; if not, write to the Free Software
 //@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //@ //////////////////////////////////////////////////////////////////////////
 
